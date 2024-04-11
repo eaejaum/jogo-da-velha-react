e451e91279049319e4a74459e5cc77bddfd62c59
eaejaum <jpedroalmeida179@gmail.com>
2024-04-11 10:43:22 -0300

    commit 1

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,61 @@
+import circle_icon from '../Assets/circle.png';
+import x_icon from '../Assets/cross.png';
+import { useState } from 'react';
+import "./TicTacToe.css";
+
+let data = ["", "", "", "", "", "", "", "", ""];
+
+const TicTacToe = () => {
+
+    let [count, setCount] = useState(0);
+    let [lock, setLock] = useState(false);
+
+    const toggle = (e, num) => {
+        if(lock){
+            return 0;
+        } 
+        if(count % 2 === 0){
+            e.target.innerHTML = `<img src='${x_icon}'>`;
+            data[num] = "x";
+            setCount(++count);
+            console.log("if: "+ count);
+        } else {
+            e.target.innerHTML = `<img src='${circle_icon}'>`;
+            data[num] = "o";
+            setCount(++count);
+            console.log("else: " + count);
+        }
+    }
+    const reset = () => {
+        setCount(0);
+    }
+    
+
+    return (
+        <div className='container'>
+            <h1 className='title'>Jogo da Velha em <span>React</span></h1>
+            <div className="board">
+                <div className="row1">
+                    <div className="boxes" onClick={(e) => {toggle(e, 0)}}></div>
+                    <div className="boxes" onClick={(e) => {toggle(e, 1)}}></div>
+                    <div className="boxes" onClick={(e) => {toggle(e, 2)}}></div>   
+                </div>
+                <div className="row2">
+                    <div className="boxes" onClick={(e) => {toggle(e, 3)}}></div>
+                    <div className="boxes" onClick={(e) => {toggle(e, 4)}}></div>
+                    <div className="boxes" onClick={(e) => {toggle(e, 5)}}></div>   
+                </div>
+                <div className="row3">
+                    <div className="boxes" onClick={(e) => {toggle(e, 6)}}></div>
+                    <div className="boxes" onClick={(e) => {toggle(e, 7)}}></div>
+                    <div className="boxes" onClick={(e) => {toggle(e, 8)}}></div>   
+                </div>
+            </div>
+            <button className="reset" onClick={() => {reset()}}>
+                Reset
+            </button>
+        </div>
+    )
+}
+
+export default TicTacToe;
